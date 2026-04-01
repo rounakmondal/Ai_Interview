afb90b6033b6d7b06ae150e55891490d73b2efa8
rounakmondal <rounakmondal198@gmail.com>
2026-04-02 04:55:41 +0530

    chanfed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.interviewsathi.online',
-  appName: 'InterviewSathi',
+  appName: 'MedhaHub',
   webDir: 'dist/spa'
 };
 
